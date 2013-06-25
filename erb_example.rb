3936cb95709e942c26330f86f863402836a8fed4
MakerSquare Student <student@lab-4.att.net>
2013-06-25 15:20:28 -0500

    Testing erb example

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,25 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+w = 44
+template2 = ERB.new "The value of w is: <%= w %>"
+puts template.result(binding)
+
+puts "The value of x + w = #{x + w}"
+
+template3 = ERB.new "Here is another way to view the result: <%= x + w %>"
+puts template3.result(binding)
+
+y = {}
+temp = ERB.new  "The value of y is: <%=y %>"
+puts temp.result(binding)
+
+z = []
+temps = ERB.new  "The value of z is: <%=z %>"
+puts temps.result(binding)
+
+
+a = "Hello world"
+temporary = ERB.new  "The value of a is: <%=a %>"
+puts temporary.result(binding)
